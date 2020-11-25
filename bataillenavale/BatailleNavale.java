b1377f92a850f23046b0820d310ccfc7e32da9ad
benjjjiii <48327243+benjjjiii@users.noreply.github.com>
2020-11-25 11:01:58 +0100

    adding main
    ajout des plateaux

--- bataillenavale/BatailleNavale.java
+++ bataillenavale/BatailleNavale.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bataillenavale;
 
 /**
@@ -15,15 +10,22 @@ public class BatailleNavale {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Plateau p = new Plateau();
-        String[][] grille = new String[32][32];
-        grille = p.initGrille();
-        
-        p.initPlacementNavires();
-        
-        p.afficherPlateau();
-        
-        
+        Plateau p1 = new Plateau();
+        Plateau p2 = new Plateau();
+        Plateau p3 = new Plateau();
+
+        String[][] grilleJoueur = new String[32][32];
+        String[][] grilleApercuIA =new String[32][32];
+        String[][] grilleIA = new String[32][32];
+
+        grilleJoueur = p1.initGrille();
+        p1.initPlacementNavires();
+        grilleApercuIA = p2.initGrille();
+
+        grilleIA = p3.initGrille();
+        p3.initPlacementNavires();
+        p1.afficherPlateau();
+        System.out.println("\n");
+        p2.afficherPlateau();
     }
-    
 }
